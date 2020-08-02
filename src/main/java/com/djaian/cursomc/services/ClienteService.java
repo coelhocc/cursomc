3762c0f8583af0599bd6a5ca1a44719cdf92ddb9
Jean Coelho <coelhocc@gmail.com>
2020-08-02 10:49:41 -0300

    Put, Delete e Get para Cliente

--- src/main/java/com/djaian/cursomc/services/ClienteService.java
+++ src/main/java/com/djaian/cursomc/services/ClienteService.java
@@ -1,12 +1,19 @@
 package com.djaian.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.djaian.cursomc.domain.Cliente;
+import com.djaian.cursomc.dto.ClienteDTO;
 import com.djaian.cursomc.repositories.ClienteRepository;
+import com.djaian.cursomc.services.exceptions.DataIntegrityException;
 import com.djaian.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -24,4 +31,36 @@ public class ClienteService {
 		
 	}
 
+	public Cliente update(Cliente obj) {
+		Cliente newobj = find(obj.getId());
+		updateData(newobj, obj);
+		return repo.save(newobj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir porque há entidades relacionadas");
+		}
+	}
+	
+	public List<Cliente> findAll(){
+		return repo.findAll();
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderby, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderby);
+		return repo.findAll(pageRequest);
+	}
+	
+	public Cliente fromDTO(ClienteDTO objDTO) {
+		return new Cliente(objDTO.getId(), objDTO.getNome(), objDTO.getEmail(), null, null);
+	}
+	
+	private void updateData(Cliente newobj, Cliente obj) {
+		newobj.setNome(obj.getNome());
+		newobj.setEmail(obj.getEmail());
+	}
 }
