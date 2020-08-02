0f05866f879849e56e60e6483acc54fb6c9a2f3a
Jean Coelho <coelhocc@gmail.com>
2020-08-02 20:18:01 -0300

    Busca de produtos por nome e categorias

---
+++ src/main/java/com/djaian/cursomc/resources/utils/URL.java
@@ -0,0 +1,28 @@
+package com.djaian.cursomc.resources.utils;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.ArrayList;
+import java.util.List;
+
+public class URL {
+
+	public static List<Integer> decodeIntList(String str){
+	
+		List<Integer> list = new ArrayList<>();
+		String[] vet = str.split(",");
+		for (int i = 0; i < vet.length; i++) {
+			list.add(Integer.parseInt(vet[i]));
+		}
+		return list;
+		//return Arrays.asList(s.split(",")).stream().map(x -> Integer.parseInt(x)).collect(Collectors.toList());
+	}
+	
+	public static String decodeParam(String str) {
+		try {
+			return URLDecoder.decode(str, "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			return "";
+		}
+	}
+}
