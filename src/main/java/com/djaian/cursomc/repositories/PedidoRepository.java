d01e855855f4e1fc34bd269e2a274440e174c542
Jean Coelho <coelhocc@gmail.com>
2020-08-15 17:30:44 -0300

    Restricao de conteudo: cliente so recupera seus pedidos

--- src/main/java/com/djaian/cursomc/repositories/PedidoRepository.java
+++ src/main/java/com/djaian/cursomc/repositories/PedidoRepository.java
@@ -1,11 +1,17 @@
 package com.djaian.cursomc.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.djaian.cursomc.domain.Cliente;
 import com.djaian.cursomc.domain.Pedido;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Integer>{
 
+	@Transactional(readOnly=true)//Para redução de lock
+	Page<Pedido> findByCliente(Cliente cliente, Pageable pageRequest);
 }
--- src/main/java/com/djaian/cursomc/services/PedidoService.java
+++ src/main/java/com/djaian/cursomc/services/PedidoService.java
@@ -6,8 +6,12 @@ import java.util.Optional;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.djaian.cursomc.domain.Cliente;
 import com.djaian.cursomc.domain.ItemPedido;
 import com.djaian.cursomc.domain.PagamentoComBoleto;
 import com.djaian.cursomc.domain.Pedido;
@@ -15,6 +19,8 @@ import com.djaian.cursomc.domain.enums.EstadoPagamento;
 import com.djaian.cursomc.repositories.ItemPedidoRepository;
 import com.djaian.cursomc.repositories.PagamentoRepository;
 import com.djaian.cursomc.repositories.PedidoRepository;
+import com.djaian.cursomc.security.UserSS;
+import com.djaian.cursomc.services.exceptions.AuthorizationException;
 import com.djaian.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -67,6 +73,21 @@ public class PedidoService {
 		itemPedidoRepository.saveAll(obj.getItens());
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
+	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderby, String direction){
+		
+		UserSS user = UserService.authenticated();
+		
+		if(user==null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderby);
+		
+		Cliente cliente = clienteService.find(user.getId());
+		
+		return repo.findByCliente(cliente, pageRequest);
 	}
 
 }
