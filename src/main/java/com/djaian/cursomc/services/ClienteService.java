e0b1d3df3a1dda9667edb16fc352c4a4d51bead2
Jean Coelho <coelhocc@gmail.com>
2020-08-19 19:27:35 -0300

    Enviando imagem via endpoint

--- src/main/java/com/djaian/cursomc/services/ClienteService.java
+++ src/main/java/com/djaian/cursomc/services/ClienteService.java
@@ -1,5 +1,6 @@
 package com.djaian.cursomc.services;
 
+import java.net.URI;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -13,6 +14,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.djaian.cursomc.domain.Cidade;
 import com.djaian.cursomc.domain.Cliente;
@@ -39,6 +41,9 @@ public class ClienteService {
 	
 	@Autowired
 	private BCryptPasswordEncoder pe;
+	
+	@Autowired
+	private S3Service s3Service;
 	
 	public Cliente find(Integer id) {
 		
@@ -109,6 +114,10 @@ public class ClienteService {
 		if(objDTO.getTelefone3() != null)
 			cli.getTelefones().addAll(Arrays.asList(objDTO.getTelefone3()));
 		return cli;
+	}
+	
+	public URI uploadProfilePicture(MultipartFile multipartFile) {
+		return s3Service.uploadFile(multipartFile);
 	}
 	
 }
