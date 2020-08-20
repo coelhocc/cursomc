6b42dd07b3b7a484dfd7e3719d9029712eb2c778
Jean Coelho <coelhocc@gmail.com>
2020-08-19 23:44:34 -0300

    Usando padrao de nomes para imagens

--- src/main/java/com/djaian/cursomc/services/ClienteService.java
+++ src/main/java/com/djaian/cursomc/services/ClienteService.java
@@ -1,5 +1,6 @@
 package com.djaian.cursomc.services;
 
+import java.awt.image.BufferedImage;
 import java.net.URI;
 import java.util.Arrays;
 import java.util.List;
@@ -8,6 +9,7 @@ import java.util.Optional;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -44,6 +46,12 @@ public class ClienteService {
 	
 	@Autowired
 	private S3Service s3Service;
+
+	@Autowired
+	private ImageService imageService;
+	
+	@Value("${img.prefix.client.profile}")
+	private String prefix;
 	
 	public Cliente find(Integer id) {
 		
@@ -122,6 +130,12 @@ public class ClienteService {
 			throw new AuthorizationException("Acesso negado!");
 		}
 		
+		BufferedImage jpgImage = imageService.getJpgImageFromFile(multipartFile);
+		String fileName = prefix + user.getId() + ".jpg";
+		
+		return s3Service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), fileName, "image");
+		
+		/*
 		URI uri = s3Service.uploadFile(multipartFile);
 		
 		Optional<Cliente> cli = repo.findById(user.getId());
@@ -129,6 +143,7 @@ public class ClienteService {
 		repo.save(cli.orElse(null));
 		
 		return uri;
+		*/
 	}
 	
 }
